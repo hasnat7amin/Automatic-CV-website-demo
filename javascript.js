28abd711b033d615a67aca2d80bdb6997e013a8a
hasnat7amin <62244945+hasnat7amin@users.noreply.github.com>
2021-02-06 23:23:42 +0500

    Uploading initial data

---
+++ javascript.js
@@ -0,0 +1,41 @@
+// let contact = {
+//    name : prompt("enter your name"),
+//    email : prompt('enter your email'),
+// }
+
+document.getElementById("name").innerHTML = prompt("Enter Your Name: ");
+document.getElementById("email").innerHTML = prompt("Enter Your Email ID :");
+document.getElementById("phoneno").innerHTML = prompt("Enter Your Phone Number : ");
+document.getElementById("Address").innerHTML = prompt("Enter Your Address")
+
+document.getElementById("language1").innerHTML = prompt("Enter Your Native Language: ");
+document.getElementById("language2").innerHTML = prompt("Enter Your National Language :");
+document.getElementById("language3").innerHTML = prompt("Enter Your InterNational Language : ");
+document.getElementById("language4").innerHTML = prompt("Enter Your 2nd Native Language :")
+
+document.getElementById("skill1").innerHTML = prompt("Enter Your 1st Skill :")
+document.getElementById("skill2").innerHTML = prompt("Enter Your 2nd Skill :")
+document.getElementById("skill3").innerHTML = prompt("Enter Your 3rd Skill :")
+document.getElementById("skill4").innerHTML = prompt("Enter Your 4th Skill :")
+document.getElementById("skill5").innerHTML = prompt("Enter Your 5th Skill :")
+document.getElementById("skill6").innerHTML = prompt("Enter Your 6th Skill :")
+
+document.getElementById("summary").innerHTML = prompt("Write a paragraph on your skillset :")
+
+document.getElementById("project1").innerHTML = prompt("Enter Your 1st Recent Completed project :")
+document.getElementById("project2").innerHTML = prompt("Enter Your 2nd Recent Completed project :")
+document.getElementById("project3").innerHTML = prompt("Enter Your 3rd Recent Completed project :")
+document.getElementById("project4").innerHTML = prompt("Enter Your 4th Recent Completed project :")
+document.getElementById("project5").innerHTML = prompt("Enter Your 5th Recent Completed project :")
+document.getElementById("project6").innerHTML = prompt("Enter Your 6th Recent Completed project :")
+
+document.getElementById("experience").innerHTML = prompt("Write a paragraph on your previous experince :")
+
+document.getElementById("education").innerHTML = prompt("Write a paragraph on your education :")
+
+document.getElementById("hire").innerHTML = prompt("Write a paragraph on why company hire you?")
+
+
+
+
+
